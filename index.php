31626a89ba8f5b5991e839219c29eaf146fa475a
Top dev <rbeer199525@gmail.comgit>
2016-01-19 11:11:50 +1000

    Updates

--- index.php
+++ index.php
@@ -1,75 +1,15 @@
 <?php get_header(); ?>
+<?php include('inc-edit.php');?>
 
-	<div id="main" class="center">
+<?php while ( have_posts() ) : the_post(); ?>
 
-		<div id="bcont">
-
-			<?php if( is_search() ) : ?>
-
-				<h2 class="btitle">Search results for <span class="sres"><?php echo $s; ?></span> <?php $my_query = new WP_Query("post_type=post&s=$s&showposts=-1"); echo $my_query->post_count; ?> hits</h2>
-				<?php get_template_part('bloghead');?>
-
-			<?php elseif( is_archive() ) : ?>
-
-				<h2 class="btitle">Posts in the <?php single_cat_title( ) ?> category</h2>
-				<?php get_template_part('bloghead');?>
-
-			<?php else : ?>
-
-				<?php get_template_part('bloghead');?>
-
-			<?php endif; ?>
-
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
-
-					<?php get_template_part('blogdets');?>
-
-					<div class="content_wrap">
-						<div class="content_style">
-
-							<?php if ( is_search() ) {
-								the_excerpt();
-							} else {
-								?>
-
-								<?/*<h2><?php the_title();?></h2>*/?>
-
-								<div class="blog_feature">
-									<?php the_post_thumbnail( 'feature_image' ); ?>
-								</div>
-
-								<div class="blog_contwrap">
-									<?php the_content();?>
-								</div>
-
-								<?
-								wp_link_pages('before=<div class="p_navigation"> Pages &after=</div>');
-
-							} ?>
-						</div>
-					</div>
-
-				</article>
-
-				<div class="clear"></div>
-
-			<?php endwhile; // End the loop. Whew. ?>
-
+	<div class="banner">
+		<div class="container skinny">
+			<h1><?php the_title(); ?></h1>
+			<?php the_content();?>
 		</div>
-
-			<nav class="navigation">
-
-				<?php wp_link_pages(); ?>
-				<div class="nav-next"><?php next_posts_link( 'Next'); ?></div>
-				<div class="nav-previous"><?php previous_posts_link('Prev'); ?></div>
-
-			</nav>
-
-			<div class="clear"></div>
-
 	</div>
 
-<?php  get_footer(); ?>
+<?php endwhile; // End the loop. Whew. ?>
+
+<?php get_footer(); ?>
--- page.php
+++ page.php
@@ -5,6 +5,7 @@
 
 	<div class="banner">
 		<div class="container skinny">
+			<h1><?php the_title(); ?></h1>
 			<?php the_content();?>
 		</div>
 	</div>
