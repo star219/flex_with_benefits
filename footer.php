b35d97b2b0a18a29367e3c4deb261bd51b8038a2
Top dev <rbeer199525@gmail.comgit>
2016-01-19 11:25:06 +1000

    Footer Layout

--- footer.php
+++ footer.php
@@ -1,19 +1,17 @@
-	
-	<div id="foot_wrap">
-		<footer class="center">	
-			<p id="copy" >Created by the Wizardz of Thrive &copy; <?php echo date('Y'); ?> All rights Reserved.</p>
-			
-			<ul id="social">
-				<li class="icon" id="facebook"><a href="#"><i class="fa fa-facebook"></i></a></li>
-				<li class="icon" id="twitter"><a href="#"><i class="fa fa-twitter"></i></a></li>
-				<li class="icon" id="instagram"><a href="#"><i class="fa fa-instagram"></i></a></li>
-				<li class="icon" id="pinterest"><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
-				<li class="icon" id="mail"><a href="#"><i class="fa fa-envelope-o"></i></a></li>
-			</ul>
-		</footer>
-	</div>
-	
+
+	<footer class="container">
+
+		<ul id="social">
+			<li class="icon" id="facebook"><a href="#"><i class="fa fa-facebook"></i></a></li>
+			<li class="icon" id="twitter"><a href="#"><i class="fa fa-twitter"></i></a></li>
+			<li class="icon" id="instagram"><a href="#"><i class="fa fa-instagram"></i></a></li>
+			<li class="icon" id="pinterest"><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
+			<li class="icon" id="mail"><a href="#"><i class="fa fa-envelope-o"></i></a></li>
+		</ul>
+		<p id="copy" >Created by the Wizardz of Thrive &copy; <?php echo date('Y'); ?> All rights Reserved.</p>
+	</footer>
+
 	<?php wp_footer(); ?>
-	
+
 </body>
 </html>
