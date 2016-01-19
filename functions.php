91eeaf39316b69c8f34588391ed1197773ea8ef5
Top dev <rbeer199525@gmail.comgit>
2016-01-19 01:33:15 +0000

    Image Sizes

--- functions.php
+++ functions.php
@@ -352,37 +352,25 @@ if (function_exists('register_sidebar')){
 }
 
 ///////////////////////////////////////////////////////
-add_theme_support( 'post-thumbnails' );
-add_image_size( 'page_thumbnail', 100, 0, false );
-add_image_size( 'feature_image', 1100, 0, false );
-
-add_image_size( 'list_image', 250, 250, true );
-add_image_size( 'rcol-image', 450, 0, false );
-
-add_image_size( 'gallery-thumb', 600, 0, false );
-add_image_size( 'lcol_image', 900, 0, false );
-add_image_size( 'wide_single', 1400, 0, false );
-add_image_size( 'owl_slide', 1200, 800, true );
-add_image_size( 'feature_image', 1100, 600, true );
 
 // add_image_size( '100x100', 100, 100, true );
-// add_image_size( '100w', 100, 0, false );
+add_image_size( '100w', 100, 0, false );
 // add_image_size( '200x200', 200, 200, true );
-// add_image_size( '200w', 200, 0, false );
+add_image_size( '200w', 200, 0, false );
 // add_image_size( '400x400', 400, 400, true );
-// add_image_size( '400w', 400, 0, false );
+add_image_size( '400w', 400, 0, false );
 // add_image_size( '600x600', 600, 600, true );
-// add_image_size( '600w', 600, 0, false );
+add_image_size( '600w', 600, 0, false );
 // add_image_size( '800x800', 800, 800, true );
-// add_image_size( '800w', 800, 0, false );
+add_image_size( '800w', 800, 0, false );
 // add_image_size( '1000x1000', 1000, 1000, true );
 // add_image_size( '1000w', 1000, 0, false );
 // add_image_size( '1200x1200', 1200, 1200, true );
-// add_image_size( '1200w', 1200, 0, false );
+add_image_size( '1200w', 1200, 0, false );
 // add_image_size( '1500x1500', 1500, 1500, true );
 // add_image_size( '1500w', 1500, 0, false );
 // add_image_size( '1800x1800', 1800, 1800, true );
-// add_image_size( '1800w', 1800, 0, false );
+add_image_size( '1800w', 1800, 0, false );
 
 //////////////////////////////////////////////////////
 // IMAGE QUALITY
