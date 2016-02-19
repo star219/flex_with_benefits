537d2b6d0871ab8326b645057826faeeaa2c126b
Top dev <rbeer199525@gmail.comgit>
2016-02-19 16:54:26 +1000

    Added Autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync');
 var sass        = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 var reload      = browserSync.reload;
 
 var src = {
@@ -19,6 +20,7 @@ gulp.task('serve', ['sass'], function() {
 
   gulp.watch(src.scss, ['sass']);
   gulp.watch(src.php).on('change', reload);
+  gulp.watch(src.js).on('change', reload);
 });
 
 // Compile sass into CSS
@@ -26,9 +28,13 @@ gulp.task('sass', function() {
   return gulp.src(src.scss)
     .pipe(sass({outputStyle: 'expanded'})
     .on('error', function(err){
-        browserSync.notify(err.message, 3000);
-        this.emit('end');
+      browserSync.notify(err.message, 3000);
+      this.emit('end');
     }))
+    .pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: true
+		}))
     .pipe(gulp.dest(src.css))
     .pipe(reload({stream: true}));
 });
