6291b6cd4bec3aaf44682136a653f657906b295d
Top dev <rbeer199525@gmail.comgit>
2016-03-15 11:05:27 +1000

    Better Inc-edit

--- inc-edit.php
+++ inc-edit.php
@@ -1,11 +1,20 @@
-<?php 
+<?php
 
-if ( is_user_logged_in() ) : 
-	
+if ( is_user_logged_in() ) :
+
 	global $current_user;
-	get_currentuserinfo();						 
-	edit_post_link( 'Edit Page', '<p class="edit_link"> Logged in as '. $current_user->user_login . '<span></span>', '</p>' ); 
+	get_currentuserinfo();
 
-endif; 
+	if (is_category()){
+			edit_term_link( 'Edit Page', '<p class="edit_link"> Logged in as '. $current_user->user_login . '<span></span>','</p>', $category, true );
+	} elseif (is_tax()){
+			edit_term_link( 'Edit Page', '<p class="edit_link"> Logged in as '. $current_user->user_login . '<span></span>','</p>', $category, true );
+	} elseif (is_tag()){
+		edit_tag_link( 'Edit Page', '<p class="edit_link"> Logged in as '. $current_user->user_login . '<span></span>','</p>', $category, true  );
+	} else {
+		edit_post_link( 'Edit Page', '<p class="edit_link"> Logged in as '. $current_user->user_login . '<span></span>', '</p>' );
+	}
+
+endif;
 
 ?>
