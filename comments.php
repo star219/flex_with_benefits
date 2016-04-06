79f580dfd68c459f22de7f675e24ba08dd9ee20f
Top dev <rbeer199525@gmail.comgit>
2016-04-06 11:06:55 +1000

    updated comments.php from twentyfifteen

--- comments.php
+++ comments.php
@@ -1,88 +1,44 @@
 <?php
-	// Do not delete these lines	
-	if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
-		die ('Please do not load this page directly. Thanks!');
-
-	if ( post_password_required() ) { ?>
-		<p class="nocomments">This post is password protected. Enter the password to view comments.</p>
-	<?php
-		return;
-	}
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
+if ( post_password_required() ) {
+	return;
+}
 ?>
 
-<!-- You can start editing here. -->
-<?php if ( have_comments() ) : ?>
-	
-	<div class="clear"></div>
+<div id="comments" class="comments-area">
 
-	<h3 id="comments_num"><?php comments_number('No Comments', 'One Comment', '% Comments' );?></h3>
-	
-	<div class="clear"></div>
+	<?php if ( have_comments() ) : ?>
+		<h2 class="comments-title">
+			<?php
+				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'twentyfifteen' ),
+					number_format_i18n( get_comments_number() ), get_the_title() );
+			?>
+		</h2>
 
-	<ul class="commentlist">
-		<?php wp_list_comments('type=comment&callback=my_comment'); ?>
-	</ul>
+		<ol class="comment-list">
+			<?php
+				wp_list_comments( array(
+					'style'       => 'ol',
+					'short_ping'  => true,
+					'avatar_size' => 56,
+				) );
+			?>
+		</ol><!-- .comment-list -->
 
 
-<?php else : // this is displayed if there are no comments so far ?>
+	<?php endif; // have_comments() ?>
 
-	<?php if ( comments_open() ) : ?>
-		<!-- If comments are open, but there are no comments. -->
-
-	 <?php else : // comments are closed ?>
-		<!-- If comments are closed. -->
-		<p class="nocomments">Comments are closed.</p>
-
+	<?php
+		// If comments are closed and there are comments, let's leave a little note, shall we?
+		if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+	?>
+		<p class="no-comments">Comments are closed.</p>
 	<?php endif; ?>
-	
-<?php endif; ?>
 
+	<?php comment_form(); ?>
 
-<?php if ( comments_open() ) : ?>
-	
-	<div id="respond" class="column750">
-	
-		<h3><?php comment_form_title( 'Leave a Reply', 'Leave a Reply to %s' ); ?></h3>
-		
-		<div class="cancel-comment-reply">
-			<?php cancel_comment_reply_link(); ?>
-		</div> 
-		
-		<?php if ( get_option('comment_registration') && !is_user_logged_in() ) : ?>
-		<p>You must be <a href="<?php echo wp_login_url( get_permalink() ); ?>">logged in</a> to post a comment.</p>
-		<?php else : ?>
-		
-		<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
-		
-			<?php if ( is_user_logged_in() ) : ?>
-			
-			<p>Logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="Log out of this account">Log out &raquo;</a></p>
-			
-			<?php else : ?>
-			
-			<p><input type="text" name="author" id="author" value="<?php echo esc_attr($comment_author); ?>" size="22" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> />
-			<label for="author">Name <?php if ($req) echo "(required)"; ?></label></p>
-			
-			<p><input type="text" name="email" id="email" value="<?php echo esc_attr($comment_author_email); ?>" size="22" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> />
-			<label for="email">Mail (will not be published) <?php if ($req) echo "(required)"; ?></label></p>
-			
-			<p><input type="text" name="url" id="url" value="<?php echo esc_attr($comment_author_url); ?>" size="22" tabindex="3" />
-			<label for="url">Website</label></p>
-			
-			<?php endif; ?>
-			
-			<!--<p><strong>XHTML:</strong> You can use these tags: <code><?php echo allowed_tags(); ?></code></p>-->
-			
-			<p><textarea name="comment" id="comment" cols="100%" rows="5" tabindex="4"></textarea></p>
-			
-			<p><input name="submit" type="submit" id="submit" tabindex="5" value="Submit Comment" />
-			<?php comment_id_fields(); ?>
-			</p>
-			<?php do_action('comment_form', $post->ID); ?>
-		
-		</form>
-		
-		<?php endif; // If registration required and not logged in ?>
-	</div>
-
-<?php endif; // if you delete this the sky will fall on your head ?>
+</div><!-- .comments-area -->
