4c59f2d7d7bd33ed2c515425c0041c1b2d852b23
Top dev <rbeer199525@gmail.comgit>
2016-04-13 15:32:58 +1000

    RM 'minimal-ui' – safari error

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS" href="<?php bloginfo('rss2_url'); ?>">
 	<link href='https://fonts.googleapis.com/css?family=Roboto:400,700,400italic' rel='stylesheet' type='text/css'>
-	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, minimal-ui" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0" />
 	<?php wp_head(); ?>
 </head>
 
