c28d63fa8fb166ed18181a3586143c5aeb410d39
Top dev <rbeer199525@gmail.comgit>
2016-04-13 16:22:13 +1000

    RM get_currentuserinfo() – deprecated

--- inc-edit.php
+++ inc-edit.php
@@ -3,7 +3,6 @@
 if ( is_user_logged_in() ) :
 
 	global $current_user;
-	get_currentuserinfo();
 
 	if (is_category()){
 			edit_term_link( 'Edit Page', '<p class="edit_link"> Logged in as '. $current_user->user_login . '<span></span>','</p>', null, true );
