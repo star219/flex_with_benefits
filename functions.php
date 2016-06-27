dcb111e90121e2b3b6281f94a826de9620ce8a5f
Top dev <rbeer199525@gmail.comgit>
2016-06-27 11:43:38 +1000

    Add WP-LOGIN autofocus fix

--- functions.php
+++ functions.php
@@ -90,6 +90,20 @@ add_filter ( 'manage_edit-post_columns',    'prefix_remove_yoast_columns' );
 add_filter ( 'manage_edit-page_columns',    'prefix_remove_yoast_columns' );
 // add_filter ( 'manage_edit-{post_type}_columns',    'prefix_remove_yoast_columns' );
 
+// Wp-Login Autofocus Fix
+add_action("login_form", "kill_wp_attempt_focus_start");
+function kill_wp_attempt_focus_start() {
+    ob_start("kill_wp_attempt_focus_replace");
+}
+
+function kill_wp_attempt_focus_replace($html) {
+    return preg_replace("/d.value = '';/", "", $html);
+}
+
+add_action("login_footer", "kill_wp_attempt_focus_end");
+function kill_wp_attempt_focus_end() {
+    ob_end_flush();
+}
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////Default Functions
