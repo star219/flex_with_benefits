d66b2358786c5a53eec81f1b2871302ce7206460
Top dev <rbeer199525@gmail.comgit>
2016-08-10 14:56:10 +1000

    login logo fix

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 /// Login Logo
 function my_login_logo() { ?>
   <style type="text/css">
-    .login h1 a {
+    #login h1 a {
       background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/logo.svg);
       margin: 0 auto;
       height: 150px;
