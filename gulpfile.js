209828e9d46b5322b109a35166cec192311ef33d
Top dev <rbeer199525@gmail.comgit>
2016-09-07 13:36:02 +1000

    RM semicolons

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,30 @@
-var gulp = require('gulp');
-var bs = require('browser-sync').create();
-var sass = require('gulp-sass');
-var rucksack = require('gulp-rucksack');
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
+var gulp = require('gulp')
+var bs = require('browser-sync').create()
+var sass = require('gulp-sass')
+var rucksack = require('gulp-rucksack')
+var autoprefixer = require('gulp-autoprefixer')
+var sourcemaps = require('gulp-sourcemaps')
 
 var src = {
   scss: 'scss/**/*.scss',
   css: './',
   php: '**/**/*.php',
   js: '**/**/*.js'
-};
+}
 
-var ignore = '!node_modules/**';
+var ignore = '!node_modules/**'
 
 // Static Server + watching scss/php files
 gulp.task('serve', ['sass'], function() {
 
   bs.init({
-    proxy: "flex.dev", // use localhost:8888 for MAMP
+    proxy: 'flex.dev', // use localhost:8888 for MAMP
     open: false
-  });
+  })
 
-  gulp.watch(src.scss, ['sass']);
-  gulp.watch([src.php, src.js, ignore]).on('change', bs.reload);
-});
+  gulp.watch(src.scss, ['sass'])
+  gulp.watch([src.php, src.js, ignore]).on('change', bs.reload)
+})
 
 // Compile sass into CSS
 gulp.task('sass', function() {
@@ -34,8 +34,8 @@ gulp.task('sass', function() {
         outputStyle: 'expanded'
       })
       .on('error', function(err) {
-        bs.notify(err.message, 3000);
-        this.emit('end');
+        bs.notify(err.message, 3000)
+        this.emit('end')
       }))
     .pipe(autoprefixer({
       browsers: ['> 1% in AU']
@@ -45,6 +45,6 @@ gulp.task('sass', function() {
     .pipe(gulp.dest(src.css))
     .pipe(bs.stream({
       match: '**/*.css'
-    }));
-});
-gulp.task('default', ['serve']);
+    }))
+})
+gulp.task('default', ['serve'])
