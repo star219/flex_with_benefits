f89e8cf0a912a95b3e23d0bacbc656835dbd8d51
Top dev <rbeer199525@gmail.comgit>
2016-12-20 08:36:20 +1000

    Trim footer.php

--- footer.php
+++ footer.php
@@ -1,13 +1,6 @@
 <footer class="footer">
 	<div class="container">
-		<ul id="social">
-			<li class="icon" id="facebook"><a href="#"><i class="fa fa-facebook"></i></a></li>
-			<li class="icon" id="twitter"><a href="#"><i class="fa fa-twitter"></i></a></li>
-			<li class="icon" id="instagram"><a href="#"><i class="fa fa-instagram"></i></a></li>
-			<li class="icon" id="pinterest"><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
-			<li class="icon" id="mail"><a href="#"><i class="fa fa-envelope-o"></i></a></li>
-		</ul>
-		<p id="copy" >Created by the Wizardz of Thrive &copy; <?php echo date('Y'); ?> All rights Reserved.</p>
+		<p>&copy; <?php echo date('Y'); ?> All rights Reserved.</p>
 	</div>
 </footer>
 
