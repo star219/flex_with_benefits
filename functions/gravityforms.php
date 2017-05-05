f94498fe3de6b959c25fb958119d770ab4308230
Top dev <rbeer199525@gmail.comgit>
2017-05-05 11:03:44 +1000

    Add Gravity transparent spinner

--- functions/gravityforms.php
+++ functions/gravityforms.php
@@ -4,6 +4,6 @@ add_filter( 'gform_confirmation_anchor', '__return_true' );
 
 add_filter( 'gform_ajax_spinner_url', 'spinner_url', 10, 2 );
 function spinner_url( $image_src, $form ) {
-  return get_template_directory_uri() . '/images/spinner.gif';
+  return get_template_directory_uri() . '/images/spinner-transparent.gif';
 }
 ?>
