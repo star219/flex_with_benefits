9209e1293c6bb845451a65cb34e5c41c159929d0
Top dev <rbeer199525@gmail.comgit>
2017-06-01 14:18:03 +1000

    Update edit link

--- inc-edit.php
+++ inc-edit.php
@@ -1,19 +1,7 @@
 <?php
-
 if ( is_user_logged_in() ) :
-
-	global $current_user;
-
-	if (is_category()){
-			edit_term_link( 'Edit Page', '<p class="edit_link"> Logged in as '. $current_user->user_login . '<span></span>','</p>', null, true );
-	} elseif (is_tax()){
-			edit_term_link( 'Edit Page', '<p class="edit_link"> Logged in as '. $current_user->user_login . '<span></span>','</p>', null, true );
-	} elseif (is_tag()){
-		edit_tag_link( 'Edit Page', '<p class="edit_link"> Logged in as '. $current_user->user_login . '<span></span>','</p>', null, true  );
-	} else {
-		edit_post_link( 'Edit Page', '<p class="edit_link"> Logged in as '. $current_user->user_login . '<span></span>', '</p>' );
-	}
-
+    global $current_user;
+    wp_get_current_user();
+    edit_post_link( '<i class="fa fa-pencil" aria-hidden="true"></i> <span>Edit Page</span>', '<p class="edit_link">', '</p>' );
 endif;
-
 ?>
